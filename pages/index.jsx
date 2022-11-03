67ed214e08bdd84030448075c4c634fca6b5b2e8
Diana1809 <diana.costache1809@gmail.com>
2022-11-03 19:57:44 +0200

    host test

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,11 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-  <div>
-    Hello
-    <i className="fa-solid fa-house"></i>
-  </div>
+    <div>
+      Hello
+      <i className="fa-solid fa-house"></i>
+      <img src="/images/logo.jpg" alt="Alt"></img>
+    </div>
   );
 }
