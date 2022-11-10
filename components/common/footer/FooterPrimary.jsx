d2dada12000ba2ba47ac8d5c5022d7a265a2928c
Diana1809 <diana.costache1809@gmail.com>
2022-11-10 22:04:48 +0200

    Curs 10.11

---
+++ components/common/footer/FooterPrimary.jsx
@@ -0,0 +1,188 @@
+import Link from 'next/link';
+
+export default function FooterPrimary() {
+  return (
+    <div className="container footer-primary-inner">
+      <nav className="footer-nav">
+        <ul>
+          <li>
+            <Link href="/" title="Information">
+              Information
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Contact Us">
+              Contact Us
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Track My Order">
+              Track My Order
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Shipping">
+              Shipping
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Free Returns">
+              Free Returns
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="My Account">
+              My Account
+            </Link>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link href="/" title="Information">
+              Information
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Contact Us">
+              Contact Us
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Track My Order">
+              Track My Order
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Shipping">
+              Shipping
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Free Returns">
+              Free Returns
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="My Account">
+              My Account
+            </Link>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link href="/" title="Information">
+              Information
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Contact Us">
+              Contact Us
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Track My Order">
+              Track My Order
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Shipping">
+              Shipping
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Free Returns">
+              Free Returns
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="My Account">
+              My Account
+            </Link>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link href="/" title="Information">
+              Information
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Contact Us">
+              Contact Us
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Track My Order">
+              Track My Order
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Shipping">
+              Shipping
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Free Returns">
+              Free Returns
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="My Account">
+              My Account
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <section className="footer-contact">Add and style contact</section>
+
+      <nav className="footer-support">
+        <ul>
+          <li>
+            <a href="tel:+07123456" title="Call us">
+              <i className="fa-solid fa-phone"></i>
+              Support
+            </a>
+          </li>
+
+          <li>
+            <a href="tel:+07123456" title="Call us">
+              <i className="fa-solid fa-phone"></i>
+              Chat
+            </a>
+          </li>
+
+          <li>
+            <a href="tel:+07123456" title="Call us">
+              <i className="fa-solid fa-phone"></i>
+              Email
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
