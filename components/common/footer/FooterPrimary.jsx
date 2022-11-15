39563b0bf27f852055144d99104cc0217f4535da
Diana1809 <diana.costache1809@gmail.com>
2022-11-15 22:09:21 +0200

    Curs 15.11

--- components/common/footer/FooterPrimary.jsx
+++ components/common/footer/FooterPrimary.jsx
@@ -44,120 +44,128 @@ export default function FooterPrimary() {
 
         <ul>
           <li>
-            <Link href="/" title="Information">
-              Information
+            <Link href="/" title="Services">
+              Services
             </Link>
           </li>
 
           <li>
-            <Link href="/" title="Contact Us">
-              Contact Us
+            <Link href="/" title="Ship to Store">
+              Ship to Store
             </Link>
           </li>
 
           <li>
-            <Link href="/" title="Track My Order">
-              Track My Order
+            <Link href="/" title="Gift Card">
+              Gift Card
             </Link>
           </li>
 
           <li>
-            <Link href="/" title="Shipping">
-              Shipping
+            <Link href="/" title="Online Only">
+              Online Only
             </Link>
           </li>
 
           <li>
-            <Link href="/" title="Free Returns">
-              Free Returns
+            <Link href="/" title="Tips & Tricks">
+              Tips & Tricks
             </Link>
           </li>
 
           <li>
-            <Link href="/" title="My Account">
-              My Account
-            </Link>
-          </li>
-        </ul>
-
-        <ul>
-          <li>
-            <Link href="/" title="Information">
-              Information
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/" title="Contact Us">
-              Contact Us
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/" title="Track My Order">
-              Track My Order
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/" title="Shipping">
-              Shipping
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/" title="Free Returns">
-              Free Returns
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/" title="My Account">
-              My Account
+            <Link href="/" title="Home">
+              Home
             </Link>
           </li>
         </ul>
 
         <ul>
           <li>
-            <Link href="/" title="Information">
-              Information
+            <Link href="/" title="Loyalty Programs">
+              Loyalty Programs
             </Link>
           </li>
 
           <li>
-            <Link href="/" title="Contact Us">
-              Contact Us
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/" title="Track My Order">
-              Track My Order
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/" title="Shipping">
-              Shipping
-            </Link>
-          </li>
-
-          <li>
-            <Link href="/" title="Free Returns">
-              Free Returns
+            <Link href="/" title="Sign up to the Simons">
+              Sign up to the Simons
             </Link>
           </li>
 
           <li>
             <Link href="/" title="My Account">
               My Account
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Conditions">
+              Conditions
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="FAQ">
+              FAQ
+            </Link>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <Link href="/" title="Careers">
+              Careers
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="About Us">
+              About Us
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Our Story">
+              Our Story
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Art & Culture">
+              Art & Culture
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Our Private Labels">
+              Our Private Labels
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/" title="Stores">
+              Stores
             </Link>
           </li>
         </ul>
       </nav>
 
-      <section className="footer-contact">Add and style contact</section>
+      <section className="footer-contact">
+        <h1>
+          <span>Thread Affair</span> Milano
+        </h1>
+
+        <p>
+          Largo Augusto 3, Milano, 20122, Milano, IT
+          <a href="tel:0039 02 760 003 66" title="Call us">
+            Tel: 0039 02 760 003 66
+          </a>
+          <a href="mailto:" title="Email us">
+            office@thread-affair.com
+          </a>
+        </p>
+      </section>
 
       <nav className="footer-support">
         <ul>
@@ -169,15 +177,15 @@ export default function FooterPrimary() {
           </li>
 
           <li>
-            <a href="tel:+07123456" title="Call us">
-              <i className="fa-solid fa-phone"></i>
+            <a href="/" target="_blank" rel="noopener noreferrer" title="Chat">
+              <i className="fa-regular fa-comments"></i>
               Chat
             </a>
           </li>
 
           <li>
-            <a href="tel:+07123456" title="Call us">
-              <i className="fa-solid fa-phone"></i>
+            <a href="mailto:office@thread-affair.com">
+              <i className="fa-solid fa-envelope-open-text"></i>
               Email
             </a>
           </li>
